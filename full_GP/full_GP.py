be0b6236465ceff1e8dd0cd3ce9e99c6f7de3d1b
YK Foong <ykfoong@gan.eng.cam.ac.uk>
2019-02-15 15:55:47 +0000

    Added the 'subset of datapoints' experiment and plotted the KL to the true posterior as a function of number of datapoints used, as in Fig 2b) in Titsias

--- full_GP/full_GP.py
+++ full_GP/full_GP.py
@@ -26,7 +26,7 @@ class GP(nn.Module):
         squared_distances = (train_inputs_col - train_inputs_row)**2        
         length_factors = (1/(2*torch.exp(self.logl2))).reshape(self.no_inputs,1,1)        
         Knn = torch.exp(self.logsigmaf2) * torch.exp(-torch.sum(length_factors * squared_distances, 0))
-        
+
         # cholesky decompose
         L = torch.potrf(Knn + torch.exp(self.logsigman2)*torch.eye(train_inputs.shape[0]), upper=False) # lower triangular decomposition
         Lslashy = torch.trtrs(train_outputs, L, upper=False)[0]
@@ -75,7 +75,7 @@ class GP(nn.Module):
             pred_var[i] = torch.exp(self.logsigmaf2) - torch.dot(v,v) + torch.exp(self.logsigman2)
         return pred_mean, pred_var
 
-    def joint_posterior_predictive(self, train_inputs, train_outputs, test_inputs, noise=True):
+    def joint_posterior_predictive(self, train_inputs, train_outputs, test_inputs, noise=False):
         # return the joint posterior, which is a multivariate Gaussian
         no_test = test_inputs.shape[0]
         # get training inputs covariance matrix
@@ -102,6 +102,25 @@ class GP(nn.Module):
 
         return pred_mean, pred_cov
 
+def gaussian_KL(mu0, mu1, Sigma0, Sigma1):
+    # calculate the KL divergence between multivariate Gaussians KL(0||1)
+    no_dims = Sigma0.shape[0]
+
+    L1 = torch.potrf(Sigma1, upper=False)
+    L1slashSigma = torch.trtrs(Sigma0 ,L1 ,upper=False)[0]
+    SigmainvSigma = torch.trtrs(L1slashSigma ,L1.transpose(0,1))[0]
+    trace_term = torch.trace(SigmainvSigma)
+
+    mu_diff = mu1 - mu0
+    v = torch.trtrs(mu_diff, L1, upper=False)[0]
+    quadratic_term = v.transpose(0,1) @ v
+
+    L0 = torch.potrf(Sigma0, upper=False)
+    logdet_term = 2*torch.sum(torch.log(torch.diag(L1))) - 2*torch.sum(torch.log(torch.diag(L0)))
+
+    KL = 0.5*(trace_term + quadratic_term - no_dims + logdet_term)
+    return KL
+
 if __name__ == "__main__":
     # set random seed for reproducibility
     torch.manual_seed(0)
@@ -113,7 +132,7 @@ if __name__ == "__main__":
     BFGS = True # use Adam or BFGS 
 
     # load the 1D dataset
-    data_location = '..//data//1D//1D_200.pkl'
+    data_location = '..//data//1D//1D_20.pkl'
     with open(data_location, 'rb') as f:
         train_inputs, train_outputs, test_inputs = pickle.load(f)
     no_train = train_outputs.size
@@ -166,7 +185,7 @@ if __name__ == "__main__":
     plt.plot(torch.squeeze(test_inputs).data.numpy(), pred_mean, color='b')
     plt.fill_between(torch.squeeze(test_inputs).data.numpy(), pred_mean + 2*pred_sd, 
             pred_mean - 2*pred_sd, color='b', alpha=0.3)
-    filepath = 'full_GP_200.pdf'
+    filepath = 'full_GP_20.pdf'
     fig.savefig(filepath)
     plt.close()
 
@@ -175,7 +194,7 @@ if __name__ == "__main__":
     pred_mean = torch.squeeze(pred_mean)
     pred_mean = pred_mean.data.numpy()
     pred_cov = pred_cov.data.numpy()
-    import pdb; pdb.set_trace()
+
     no_samples = 10
     # sample from the posterior predictive
     samples = np.random.multivariate_normal(pred_mean, pred_cov, no_samples)
@@ -184,12 +203,12 @@ if __name__ == "__main__":
     plt.plot(torch.squeeze(train_inputs).data.numpy(), train_outputs.data.numpy(), '+k')
     for i in range(no_samples):
         plt.plot(torch.squeeze(test_inputs).data.numpy(), samples[i,:])
-    filepath = 'full_GP_200_samples.pdf'
+    filepath = 'full_GP_20_samples.pdf'
     fig.savefig(filepath)
     plt.close()
 
     # print final NLL and hyperparameters
-    file = open('results_200.txt','w') 
+    file = open('results_20.txt','w') 
     file.write('1D GP regression with 20 points \n')
     file.write('noise_sd: {} \n'.format(torch.exp(model.logsigman2/2).item()))
     file.write('function_sd: {} \n'.format(torch.exp(model.logsigmaf2/2).item()))
--- full_GP/subset_data_GP_boston.py
+++ full_GP/subset_data_GP_boston.py
@@ -1,6 +1,7 @@
-# train a full GP on a subset of the data of Boston Housing. Compare the results with the full GP as the number of 'inducing points' increases
+# train full GP subsets of the data of Boston Housing. Compare the results with the full GP as the number of 'inducing points' increases
 
 from full_GP import GP
+from full_GP import gaussian_KL
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,15 +11,70 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-def subset_data_train(no_points, x_train_normalised, y_train_normalised):
-    # train the GP on a subset of the data and return the joint predictive distribution on the test inputs
+def train(x_train_normalised, y_train_normalised, x_test, no_inputs=13, BFGS=True, learning_rate=1, no_iters=200, return_hypers=False):
+    # train the GP and return the joint predictive distribution on the test inputs
+    # initialise model
+    model = GP(no_inputs)
     
+    # optimize hyperparameters
+    if BFGS == True:
+        optimizer = optim.LBFGS(model.parameters(), lr=learning_rate)
+    else:
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    with trange(no_iters) as t:
+        for i in t:
+            if BFGS == True:
+                def closure():
+                    optimizer.zero_grad()
+                    NLL = -model.get_LL(x_train_normalised, y_train_normalised)
+                    NLL.backward()
+                    return NLL
+                optimizer.step(closure)
+                NLL = -model.get_LL(x_train_normalised, y_train_normalised)
+            else: 
+                optimizer.zero_grad()
+                NLL = -model.get_LL(x_train_normalised, y_train_normalised)
+                NLL.backward()
+                optimizer.step() 
+            # update tqdm 
+            if i % 10 == 0:
+                t.set_postfix(loss=NLL.item())
+
+    # get joint posterior predictive of the latent function values
+    pred_mean, pred_cov = model.joint_posterior_predictive(x_train_normalised, y_train_normalised, x_test, noise=False)
+    if return_hypers == True:
+        logl2 = model.logl2
+        logsigmaf2 = model.logsigmaf2
+        logsigman2 = model.logsigman2
+        return pred_mean, pred_cov, logl2, logsigmaf2, logsigman2
+    else:
+        return pred_mean, pred_cov
+
+def subset_data_predict(model, subsample_list, x_train_normalised, y_train_normalised, x_test_normalised, pred_mean_full, pred_cov_full):
+    # get the KL divergence to the true posterior as a function of subsampling
+    KL_list = np.zeros(len(subsample_list))
+    no_train = x_train_normalised.shape[0]
+    for i, no_points in enumerate(subsample_list):
+        # pick a random subset of the data
+        ind = np.random.choice(no_train, no_points, replace=False)
+        x_train_normalised_subsampled = x_train_normalised[ind,:]
+        y_train_normalised_subsampled = y_train_normalised[ind]
+
+        # get joint posterior predictive with subset of data
+        pred_mean, pred_cov = model.joint_posterior_predictive(x_train_normalised_subsampled, y_train_normalised_subsampled, x_test_normalised, noise=False)
+        
+        # calculate KL divergence between subsampled posterior and full posterior
+        KL = gaussian_KL(pred_mean_full, pred_mean, pred_cov_full, pred_cov)
+        KL_list[i] = KL.data.numpy()
+    return KL_list
 
 if __name__ == "__main__":
     # set random seed for reproducibility
     torch.manual_seed(0)
     np.random.seed(0)
+
+    directory = 'experiments//subset_data//'
 
     # unpickle the boston housing dataset
     data_location = '..//data//boston_housing//boston_housing0.pkl'
@@ -44,3 +100,40 @@ if __name__ == "__main__":
     # normalise the test inputs
     x_test_normalised = x_test - train_mean[:-1]
     x_test_normalised = x_test_normalised/train_sd[:-1]
+
+    # hyperparameters
+    no_inputs = 13
+    BFGS = True
+    learning_rate = 1
+    no_iters = 100
+
+    # train on the full data set, and keep the hyperparameters learned
+    pred_mean_full, pred_cov_full, logl2, logsigmaf2, logsigman2 = train(x_train_normalised, y_train_normalised, x_test_normalised, no_inputs, BFGS, learning_rate, no_iters, return_hypers=True)
+
+    # subsample the training set and do inference
+    subsample_list = [10, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 455]    
+    # initialise model using full GP hyperparameters
+    model = GP(no_inputs)
+    model.logl2 = logl2
+    model.logsigmaf2 = logsigmaf2
+    model.logsigman2 = logsigman2
+
+    no_exps = 10
+    KL_lists = np.zeros((no_exps, len(subsample_list))) 
+    for i in range(no_exps): # repeat the experiment 10 times
+        KL_lists[i,:] = subset_data_predict(model, subsample_list, x_train_normalised, y_train_normalised, x_test_normalised, pred_mean_full, pred_cov_full)
+    KL_mean = np.mean(KL_lists, axis=0)
+    KL_se = np.std(KL_lists, axis=0)/np.sqrt(no_exps) 
+    # ^ standard error of the mean - can't tell if Titsias uses standard deviation or 
+    # standard error in the paper - he says standard error but plot looks more like standard deviation
+
+    # plot the KL against number of subsampled points
+    fig, ax = plt.subplots()
+    plt.errorbar(np.array(subsample_list), KL_mean, yerr = KL_se, capsize=5)
+    plt.xlabel('Number of inducing variables')
+    plt.ylabel('KL(p||q)')
+    filepath = directory + 'subset_data_KL.pdf'
+    fig.savefig(filepath)
+    plt.close()
+        
+
